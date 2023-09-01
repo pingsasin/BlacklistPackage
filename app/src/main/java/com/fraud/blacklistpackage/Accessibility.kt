5844b39d94740c91088909daefe31641635411e7
Sasin Pattararittigul <54998@tmbbank.com>
2023-09-01 11:24:34 +0700

    ..

---
+++ app/src/main/java/com/fraud/blacklistpackage/Accessibility.kt
@@ -0,0 +1,14 @@
+package com.fraud.blacklistpackage
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class Accessibility: AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInterrupt() {
+        TODO("Not yet implemented")
+    }
+}
